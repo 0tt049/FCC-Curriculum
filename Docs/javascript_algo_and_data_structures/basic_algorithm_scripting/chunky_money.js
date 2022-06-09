9bb77c8c6ed757a1dc162746b883638c8d96f39c
0tt049 <otavio@otaviocoelho.com>
2022-06-09 08:36:37 -0300

    completed exercise

--- Docs/javascript_algo_and_data_structures/basic_algorithm_scripting/chunky_money.js
+++ Docs/javascript_algo_and_data_structures/basic_algorithm_scripting/chunky_money.js
@@ -5,14 +5,15 @@ Write a function that splits an array (first argument) into groups the length of
 */
 
 function chunkArrayInGroups(arr, size) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(i, arr[i]);
-
+  let arr1 = []
+  for (let i = 0; i < (arr.length); i += size) {
+    let arr0 = arr.slice(i, size + i);
+    arr1.push(arr0)
   }
-  return arr;
+  return arr1;
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+console.log(chunkArrayInGroups(["1", "2", "3", "4", "5"], 2));
 
 // a b
 // c d
